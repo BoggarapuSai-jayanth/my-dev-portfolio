f5f3d8b68805be58af57bfb123e61d244ca46f79
BoggarapuSai-jayanth <sai766932@gmail.com>
2026-03-21 01:37:17 +0530

    .

--- server/server.js
+++ server/server.js
@@ -36,6 +36,12 @@ app.get('/api/health', (req, res) => {
   });
 });
 
+app.use(cors({
+  origin: ['https://my-dev-portfolio-ruby.vercel.app', 'http://localhost:5173'],
+  credentials: true
+}));
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
